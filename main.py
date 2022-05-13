b7df3773a59132d4bd04da62818c8931da25f2a7
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-13 17:18:05 -0400

    created list of question objects from the data

---
+++ main.py
@@ -0,0 +1,20 @@
+# TODO Bring question data into the main.py file. Instead of having a list of dictionaries, make a list of question objects.
+
+from question_model import Question
+from data import question_data
+
+# TODO Create question_bank, a list of question objects
+question_bank = []
+
+# TODO Loop through ech of the questions in question_data
+for question in question_data:
+    #for each question create a variable called question_text
+    question_text = question["text"]
+    question_answer = question["answer"]
+    # TODO Create a Question object from each entry in question_data
+    new_question = Question(question_text,question_answer)
+    # TODO Append each Question object to the question_bank
+    question_bank.append(new_question)
+
+print(question_bank[0].text)
+print(question_bank[0].answer)
