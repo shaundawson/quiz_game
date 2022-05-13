389482233e6117101c7f801a9a1939c29945ee55
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-13 16:05:22 -0400

    notes- working with attributes

--- notes.py
+++ notes.py
@@ -7,4 +7,15 @@ user_1 = User()
 
 
 # TODO Working with Attributes, Class Constructors and the _init_() Function
+user_1.id = "001"
+user_1.user_name = "Shaun"
+
+
+print(user_1.user_name)
+
+user_2 = User()
+user_2.id = "002"
+user_2.user_name = "Jack"
+
+
 # TODO Adding Methods to a Class
