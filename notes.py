de8bf92f8c2729949c6cb9947dd072eff06865e8
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-13 16:29:40 -0400

    notes- adding methods to a class

--- notes.py
+++ notes.py
@@ -4,11 +4,18 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+   # TODO Adding Methods to a Class
+    def follow(self, user):
+        user.followers +=1
+        self.following +=1
 
 
 user_1 = User("001","Shaun")
 user_2 = User("002","John")
+
+user_1.follow(user_2)
 print(user_1.followers)
-
-
-# TODO Adding Methods to a Class
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
